da6ce3b323b44697049654c5a9acb2ab17dba95c
maheshreddy7797 <maheshreddy7797@gmail.com>
2017-06-14 00:22:44 +0530

    Create test casec for all   functions.

--- driver_test.go
+++ driver_test.go
@@ -44,3 +44,235 @@ func Test_Create(t *testing.T) {
 	}
 
 }
+
+func TestExampleDriver_Create(t *testing.T) {
+	type fields struct {
+		volumes    map[string]string
+		m          *sync.Mutex
+		mountPoint string
+	}
+	type args struct {
+		r volume.Request
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   volume.Response
+	}{
+	// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		d := ExampleDriver{
+			volumes:    tt.fields.volumes,
+			m:          tt.fields.m,
+			mountPoint: tt.fields.mountPoint,
+		}
+		if got := d.Create(tt.args.r); !reflect.DeepEqual(got, tt.want) {
+			t.Errorf("%q. ExampleDriver.Create() = %v, want %v", tt.name, got, tt.want)
+		}
+	}
+}
+
+func TestExampleDriver_List(t *testing.T) {
+	type fields struct {
+		volumes    map[string]string
+		m          *sync.Mutex
+		mountPoint string
+	}
+	type args struct {
+		r volume.Request
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   volume.Response
+	}{
+	// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		d := ExampleDriver{
+			volumes:    tt.fields.volumes,
+			m:          tt.fields.m,
+			mountPoint: tt.fields.mountPoint,
+		}
+		if got := d.List(tt.args.r); !reflect.DeepEqual(got, tt.want) {
+			t.Errorf("%q. ExampleDriver.List() = %v, want %v", tt.name, got, tt.want)
+		}
+	}
+}
+
+func TestExampleDriver_Get(t *testing.T) {
+	type fields struct {
+		volumes    map[string]string
+		m          *sync.Mutex
+		mountPoint string
+	}
+	type args struct {
+		r volume.Request
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   volume.Response
+	}{
+	// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		d := ExampleDriver{
+			volumes:    tt.fields.volumes,
+			m:          tt.fields.m,
+			mountPoint: tt.fields.mountPoint,
+		}
+		if got := d.Get(tt.args.r); !reflect.DeepEqual(got, tt.want) {
+			t.Errorf("%q. ExampleDriver.Get() = %v, want %v", tt.name, got, tt.want)
+		}
+	}
+}
+
+func TestExampleDriver_Remove(t *testing.T) {
+	type fields struct {
+		volumes    map[string]string
+		m          *sync.Mutex
+		mountPoint string
+	}
+	type args struct {
+		r volume.Request
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   volume.Response
+	}{
+	// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		d := ExampleDriver{
+			volumes:    tt.fields.volumes,
+			m:          tt.fields.m,
+			mountPoint: tt.fields.mountPoint,
+		}
+		if got := d.Remove(tt.args.r); !reflect.DeepEqual(got, tt.want) {
+			t.Errorf("%q. ExampleDriver.Remove() = %v, want %v", tt.name, got, tt.want)
+		}
+	}
+}
+
+func TestExampleDriver_Path(t *testing.T) {
+	type fields struct {
+		volumes    map[string]string
+		m          *sync.Mutex
+		mountPoint string
+	}
+	type args struct {
+		r volume.Request
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   volume.Response
+	}{
+	// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		d := ExampleDriver{
+			volumes:    tt.fields.volumes,
+			m:          tt.fields.m,
+			mountPoint: tt.fields.mountPoint,
+		}
+		if got := d.Path(tt.args.r); !reflect.DeepEqual(got, tt.want) {
+			t.Errorf("%q. ExampleDriver.Path() = %v, want %v", tt.name, got, tt.want)
+		}
+	}
+}
+
+func TestExampleDriver_Mount(t *testing.T) {
+	type fields struct {
+		volumes    map[string]string
+		m          *sync.Mutex
+		mountPoint string
+	}
+	type args struct {
+		r volume.MountRequest
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   volume.Response
+	}{
+	// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		d := ExampleDriver{
+			volumes:    tt.fields.volumes,
+			m:          tt.fields.m,
+			mountPoint: tt.fields.mountPoint,
+		}
+		if got := d.Mount(tt.args.r); !reflect.DeepEqual(got, tt.want) {
+			t.Errorf("%q. ExampleDriver.Mount() = %v, want %v", tt.name, got, tt.want)
+		}
+	}
+}
+
+func TestExampleDriver_Unmount(t *testing.T) {
+	type fields struct {
+		volumes    map[string]string
+		m          *sync.Mutex
+		mountPoint string
+	}
+	type args struct {
+		r volume.UnmountRequest
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   volume.Response
+	}{
+	// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		d := ExampleDriver{
+			volumes:    tt.fields.volumes,
+			m:          tt.fields.m,
+			mountPoint: tt.fields.mountPoint,
+		}
+		if got := d.Unmount(tt.args.r); !reflect.DeepEqual(got, tt.want) {
+			t.Errorf("%q. ExampleDriver.Unmount() = %v, want %v", tt.name, got, tt.want)
+		}
+	}
+}
+
+func TestExampleDriver_Capabilities(t *testing.T) {
+	type fields struct {
+		volumes    map[string]string
+		m          *sync.Mutex
+		mountPoint string
+	}
+	type args struct {
+		r volume.Request
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   volume.Response
+	}{
+	// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		d := ExampleDriver{
+			volumes:    tt.fields.volumes,
+			m:          tt.fields.m,
+			mountPoint: tt.fields.mountPoint,
+		}
+		if got := d.Capabilities(tt.args.r); !reflect.DeepEqual(got, tt.want) {
+			t.Errorf("%q. ExampleDriver.Capabilities() = %v, want %v", tt.name, got, tt.want)
+		}
+	}
+}
