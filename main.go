3add897b949dd9ba0aca2106d5ab4e1006b0b947
Mahesh Reddy <maheshreddy7797@gmail.com>
2017-06-21 16:10:37 +0530

    Added description

--- main.go
+++ main.go
@@ -7,13 +7,13 @@ import (
 )
 
 func main() {
-
+// create the server that will serve our Unix socket for the Docker daemon. 
 	driver := NewExampleDriver()
 	handler := volume.NewHandler(driver)
 	if err := handler.ServeUnix("dvd", 0); err != nil {
 		log.Fatalf("Error %v", err)
 	}
-
+// The empty for loop is here so that the main function becomes blocking since the server will go in a separate goroutine.
 	for {
 
 	}
